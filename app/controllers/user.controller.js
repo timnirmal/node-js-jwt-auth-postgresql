2968de3ae1e8ca7946caa4a47ff9a0cf3f6dab7b
timnirmal <timnirmal@gmail.com>
2022-02-08 15:00:06 +0530

    Controller for testing Authorization

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,20 @@
+// – /api/test/all for public access
+// – /api/test/user for loggedIn users (role: user/moderator/admin)
+// – /api/test/mod for users having moderator role
+// – /api/test/admin for users having admin role
+
+exports.allAccess = (req, res) => {
+    res.status(200).send("Public Content.");
+};
+exports.userBoard = (req, res) => {
+    res.status(200).send("User Content.");
+};
+exports.adminBoard = (req, res) => {
+    res.status(200).send("Admin Content.");
+};
+exports.moderatorBoard = (req, res) => {
+    res.status(200).send("Moderator Content.");
+};
+
+
+
